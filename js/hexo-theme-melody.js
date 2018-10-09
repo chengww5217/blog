5dbc0d0c549753be2c6943db8caaf0f8b99146f1
chengww <chengwang@yunify.com>
2018-10-09 21:38:07 +0800

    Site updated: 2018-10-09 21:38:07

--- js/hexo-theme-melody.js
+++ js/hexo-theme-melody.js
@@ -1,3 +1,3 @@
-// build time:Tue Oct 09 2018 20:33:20 GMT+0800 (China Standard Time)
+// build time:Tue Oct 09 2018 21:38:02 GMT+0800 (China Standard Time)
 
 //rebuild by neat 
--- js/transition.js
+++ js/transition.js
@@ -1,3 +1,3 @@
-// build time:Tue Oct 09 2018 20:33:20 GMT+0800 (China Standard Time)
+// build time:Tue Oct 09 2018 21:38:02 GMT+0800 (China Standard Time)
 $(function(){$(".layout").velocity("stop").velocity("transition.slideUpIn",{delay:500,duration:1e3,easing:"easeInOutQuart",complete:function(){if($(".sidebar-toc").length>0){setTimeout(function(){$("#toggle-sidebar").click()},200)}}});$("#top-container").velocity("stop").velocity("transition.fadeIn",{delay:500,duration:1e3,easing:"easeInOutQuart"})});
 //rebuild by neat 
