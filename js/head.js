cf4a6308bd5639937d39439cab57681b001150dc
chengww <chengwang@yunify.com>
2018-10-11 15:17:30 +0800

    Site updated: 2018-10-11 15:17:29

--- js/head.js
+++ js/head.js
@@ -1,3 +1,3 @@
-// build time:Thu Oct 11 2018 15:11:07 GMT+0800 (China Standard Time)
+// build time:Thu Oct 11 2018 15:17:23 GMT+0800 (China Standard Time)
 $(function(){$(".toggle-menu").on("click",function(){if(!$(".menus").is(":visible")){$(".menus").velocity("stop").velocity("transition.slideDownIn",{duration:300})}else{$(".menus").velocity("stop").velocity("transition.slideUpOut",{duration:300})}});$(document).on("click",function(i){var n=$(".menus")[0].contains(i.target)||$(".toggle-menu")[0].contains(i.target);if(!n&&$(".toggle-menu").is(":visible")){$(".menus").velocity("stop").velocity("transition.slideUpOut",{duration:300})}});$(window).on("resize",function(i){if(!$(".toggle-menu").is(":visible")){if(!$(".menus").is(":visible")){$(".menus").velocity("stop").velocity("transition.slideDownIn",{duration:300})}}})});
 //rebuild by neat 
