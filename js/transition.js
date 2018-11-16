24936bd60025758122ee47d74e571b721434a4cb
chengww <chengwang@yunify.com>
2018-11-16 11:19:59 +0800

    Site updated: 2018-11-16 11:19:58

--- js/transition.js
+++ js/transition.js
@@ -1,3 +1,3 @@
-// build time:Mon Nov 12 2018 11:21:38 GMT+0800 (China Standard Time)
+// build time:Fri Nov 16 2018 11:19:50 GMT+0800 (China Standard Time)
 $(function(){$(".layout").velocity("stop").velocity("transition.slideUpIn",{delay:500,duration:1e3,easing:"easeInOutQuart",complete:function(){if($(".sidebar-toc").length>0){setTimeout(function(){$("#toggle-sidebar").click()},200)}}});$("#top-container").velocity("stop").velocity("transition.fadeIn",{delay:500,duration:1e3,easing:"easeInOutQuart"})});
 //rebuild by neat 
