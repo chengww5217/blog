d7c234d6a6a274f56b1d52ecf3b791c3966e00cb
chengww <chengwang@yunify.com>
2019-03-22 15:56:03 +0800

    Site updated: 2019-03-22 15:56:03

--- js/hexo-theme-melody.js
+++ js/hexo-theme-melody.js
@@ -1,3 +1,3 @@
-// build time:Fri Mar 22 2019 15:52:16 GMT+0800 (China Standard Time)
+// build time:Fri Mar 22 2019 15:56:01 GMT+0800 (China Standard Time)
 
 //rebuild by neat 
--- js/transition.js
+++ js/transition.js
@@ -1,3 +1,3 @@
-// build time:Fri Mar 22 2019 15:52:16 GMT+0800 (China Standard Time)
+// build time:Fri Mar 22 2019 15:56:01 GMT+0800 (China Standard Time)
 $(function(){$(".layout").velocity("stop").velocity("transition.slideUpIn",{delay:500,duration:1e3,easing:"easeInOutQuart",complete:function(){if($(".sidebar-toc").length>0){setTimeout(function(){$("#toggle-sidebar").click()},200)}}});$("#top-container").velocity("stop").velocity("transition.fadeIn",{delay:500,duration:1e3,easing:"easeInOutQuart"})});
 //rebuild by neat 
