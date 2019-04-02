2416fbc3f5ddbfa5d1afae47a292928be8c57154
chengww <chengww5217@163.com>
2019-04-02 10:47:29 +0800

    Site updated: 2019-04-02 10:47:29

--- js/transition.js
+++ js/transition.js
@@ -1,3 +1,3 @@
-// build time:Tue Mar 26 2019 22:17:54 GMT+0800 (China Standard Time)
+// build time:Tue Apr 02 2019 10:47:27 GMT+0800 (China Standard Time)
 $(function(){$(".layout").velocity("stop").velocity("transition.slideUpIn",{delay:500,duration:1e3,easing:"easeInOutQuart",complete:function(){if($(".sidebar-toc").length>0){setTimeout(function(){$("#toggle-sidebar").click()},200)}}});$("#top-container").velocity("stop").velocity("transition.fadeIn",{delay:500,duration:1e3,easing:"easeInOutQuart"})});
 //rebuild by neat 
