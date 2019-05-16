ab9008748d100bc649ae5b948639bbacc00893f3
chengww <chengww5217@163.com>
2019-05-16 11:13:20 +0800

    Site updated: 2019-05-16 11:13:20

--- js/fancybox.js
+++ js/fancybox.js
@@ -1,3 +1,3 @@
-// build time:Tue May 07 2019 10:25:31 GMT+0800 (China Standard Time)
+// build time:Thu May 16 2019 11:13:17 GMT+0800 (China Standard Time)
 $(function(){var a=$(".recent-post-item img");if(a.length===0){a=$("#post-content img")}for(var t=0;t<a.length;t++){var o=$('<a href="'+a[t].src+'" data-fancybox="group" data-caption="'+a[t].alt+'" class="fancybox"></a>');var e=a[t].alt;var n=$(a[t]).wrap(o);if(e){n.after('<div class="img-alt">'+e+"</div>")}}$().fancybox({selector:"[data-fancybox]",loop:true,transitionEffect:"slide",buttons:["share","slideShow","fullScreen","download","thumbs","close"]});var r=$(".gallery-item");var i=[];r.each(function(a,t){i.push({src:$(t).data("url"),opts:{caption:$(t).data("title")}})});r.on("click",function(){$.fancybox.open(i,{loop:true,transitionEffect:"slide"},r.index(this));return false})});
 //rebuild by neat 
