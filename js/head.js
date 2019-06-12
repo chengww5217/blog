acef74ea0d083b005b35568d4a35dbee32148918
chengww <chengww5217@163.com>
2019-06-12 16:35:15 +0800

    Site updated: 2019-06-12 16:35:15

--- js/head.js
+++ js/head.js
@@ -1,3 +1,3 @@
-// build time:Wed Jun 12 2019 16:32:29 GMT+0800 (China Standard Time)
+// build time:Wed Jun 12 2019 16:35:12 GMT+0800 (China Standard Time)
 $(function(){$(".toggle-menu").on("click",function(){if(!$(".menus").is(":visible")){$(".menus").velocity("stop").velocity("transition.slideDownIn",{duration:300})}else{$(".menus").velocity("stop").velocity("transition.slideUpOut",{duration:300})}});$(document).on("click",function(i){var n=$(".menus")[0].contains(i.target)||$(".toggle-menu")[0].contains(i.target);if(!n&&$(".toggle-menu").is(":visible")){$(".menus").velocity("stop").velocity("transition.slideUpOut",{duration:300})}});$(window).on("resize",function(i){if(!$(".toggle-menu").is(":visible")){if(!$(".menus").is(":visible")){$(".menus").velocity("stop").velocity("transition.slideDownIn",{duration:300})}}})});
 //rebuild by neat 
