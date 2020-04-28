8a38be06aca26e8e27ce435744ee49753558b71f
chengww <chengww5217@163.com>
2020-04-29 00:35:06 +0800

    Site updated: 2020-04-29 00:35:05

--- js/transition.js
+++ js/transition.js
@@ -1,3 +1,3 @@
-// build time:Wed Apr 29 2020 00:33:44 GMT+0800 (China Standard Time)
+// build time:Wed Apr 29 2020 00:35:04 GMT+0800 (China Standard Time)
 $(function(){$(".layout").velocity("stop").velocity("transition.slideUpIn",{delay:500,duration:1e3,easing:"easeInOutQuart",complete:function(){if($(".sidebar-toc").length>0){setTimeout(function(){$("#toggle-sidebar").click()},200)}}});$("#top-container").velocity("stop").velocity("transition.fadeIn",{delay:500,duration:1e3,easing:"easeInOutQuart"})});
 //rebuild by neat 
