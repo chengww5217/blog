96363c05ae97a593a8074e4f7bd9cf7ed9c447f2
chengww <chengww5217@163.com>
2020-04-28 23:13:32 +0800

    Site updated: 2020-04-28 23:13:31

--- js/head.js
+++ js/head.js
@@ -1,3 +1,3 @@
-// build time:Fri Aug 30 2019 11:55:03 GMT+0800 (China Standard Time)
+// build time:Tue Apr 28 2020 23:13:30 GMT+0800 (China Standard Time)
 $(function(){$(".toggle-menu").on("click",function(){if(!$(".menus").is(":visible")){$(".menus").velocity("stop").velocity("transition.slideDownIn",{duration:300})}else{$(".menus").velocity("stop").velocity("transition.slideUpOut",{duration:300})}});$(document).on("click",function(i){var n=$(".menus")[0].contains(i.target)||$(".toggle-menu")[0].contains(i.target);if(!n&&$(".toggle-menu").is(":visible")){$(".menus").velocity("stop").velocity("transition.slideUpOut",{duration:300})}});$(window).on("resize",function(i){if(!$(".toggle-menu").is(":visible")){if(!$(".menus").is(":visible")){$(".menus").velocity("stop").velocity("transition.slideDownIn",{duration:300})}}})});
 //rebuild by neat 
