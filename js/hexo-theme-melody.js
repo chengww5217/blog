b246109b94649810f40aef8a6c60e5eabbd450e5
chengww <chengww5217@163.com>
2022-03-26 19:20:34 +0800

    Site updated: 2022-03-26 19:20:34

--- js/hexo-theme-melody.js
+++ js/hexo-theme-melody.js
@@ -1,3 +1,3 @@
-// build time:Sat Mar 26 2022 19:18:55 GMT+0800 (China Standard Time)
+// build time:Sat Mar 26 2022 19:20:32 GMT+0800 (China Standard Time)
 
 //rebuild by neat 
--- js/transition.js
+++ js/transition.js
@@ -1,3 +1,3 @@
-// build time:Sat Mar 26 2022 19:18:55 GMT+0800 (China Standard Time)
+// build time:Sat Mar 26 2022 19:20:32 GMT+0800 (China Standard Time)
 $(function(){$(".layout").velocity("stop").velocity("transition.slideUpIn",{delay:500,duration:1e3,easing:"easeInOutQuart",complete:function(){if($(".sidebar-toc").length>0){setTimeout(function(){$("#toggle-sidebar").click()},200)}}});$("#top-container").velocity("stop").velocity("transition.fadeIn",{delay:500,duration:1e3,easing:"easeInOutQuart"})});
 //rebuild by neat 
